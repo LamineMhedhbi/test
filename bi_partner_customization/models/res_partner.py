8a381730867f9fb0dbb07fb94e9f72b06a0b96b2
laminemhedhbi <mohamed.amine.mhedhbi@gmail.com>
2022-01-26 10:37:13 +0100

    second commit

--- bi_partner_customization/models/res_partner.py
+++ bi_partner_customization/models/res_partner.py
@@ -4,9 +4,9 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    cin = fields.Many2many("ir.attachment", "partner_cin_ir_attachments_rel", string="CIN")
-    banking_receipt = fields.Many2many("ir.attachment", "partner_br_ir_attachments_rel", string="Banking Receipt")
-    payslip = fields.Many2many("ir.attachment", "partner_payslip_ir_attachments_rel", string="Payslip")
+    photo1 = fields.Many2many("ir.attachment", "partner_cin_ir_attachments_rel", string="Photo 1")
+    photo2 = fields.Many2many("ir.attachment", "partner_br_ir_attachments_rel", string="Photo 2")
+    photo3 = fields.Many2many("ir.attachment", "partner_payslip_ir_attachments_rel", string="Photo 3")
 
     def confirm_membership(self):
         pass
